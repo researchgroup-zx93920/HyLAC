2ffe32516438d7be59c379a4a012de7a30edf682
Wes Cravens <wbc3@illinois.edu>
2022-03-29 17:58:17 -0500

    opt-0 final update

--- 3rd_Party/HungarianGPU/HungarianCUDA.cu
+++ 3rd_Party/HungarianGPU/HungarianCUDA.cu
@@ -38,7 +38,6 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-// #include <device_functions.h>
 #include <cuda_runtime_api.h>
 #include <stdlib.h>
 #include <stdio.h>
